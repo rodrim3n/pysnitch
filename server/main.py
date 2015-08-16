ccfd1d54aa00493bdd04f19173b2818d3d68e260
Ivan Karl <ivan6258@gmail.com>
2015-08-16 19:02:44 -0300

    adds parameters to server script

--- server/main.py
+++ server/main.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python2.7
 from objects import *
+import sys
+import getopt
 
-HOST = "192.168.0.50"
-PORT = 2222
+# This script can receive a host and a port -> 'h:p:'
 
+try:
+    options = getopt.getopt(sys.argv[1:], 'h:p:')
+    options = dict(options[0])
+except:
+    print 'Wrong arguments received, defaulting to localhost:2222'
+    options = {}
+
+default_options = {'-h': '0.0.0.0', '-p': 2222}
+
+# Merge received arguments with default ones
+options = dict(default_options, **options)
+
+HOST = options['-h']
+PORT = options['-p']
 
 if __name__ == "__main__":
     Server(HOST, PORT).run()
