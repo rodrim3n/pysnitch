ec152cd8d834c9095c041b2632d6b894053eb01a
Rodrigo Otero <rodri.m3n@gmail.com>
2015-08-21 17:30:18 -0300

    fix test/shell

--- test/shell_test.py
+++ test/shell_test.py
@@ -1,20 +1,26 @@
 import unittest
 from server.objects import *
+from mock import patch
 
 
 class TestShell(unittest.TestCase):
 
     def setUp(self):
         self.client = ClientConnection('socket', 'adress')
+        self.client.username = 'pepe'
         self.shell = Shell(self.client)
 
-    def test_command_adds_no_args(self):
+    def test_command_parser_no_args(self):
         cmd = 'ls'
-        self.assertEqual('ls;', self.shell.command_adds(cmd))
+        self.assertEqual('ls;', self.shell.command_parser(cmd))
 
-    def test_command_adds_with_args(self):
+    def test_command_parser_single_arg(self):
         cmd = 'ls /home'
-        self.assertEqual('ls;/home;', self.shell.command_adds(cmd))
+        self.assertEqual('ls;/home;', self.shell.command_parser(cmd))
+
+    def test_command_parser_multiple_args(self):
+        cmd = 'ls /home /pepe /asd'
+        self.assertEqual('ls;/home;/pepe;/asd;', self.shell.command_parser(cmd))
 
 
 if __name__ == '__main__':
